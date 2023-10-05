f4175b9febabb70ba27b10cf7ee17edd71f310ac
jgreener6 <justinmgreen06@gmail.com>
2023-10-04 23:47:44 -0400

    Initial set up

---
+++ scheduleme-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function MyComponent() {
+  return (
+    <div>
+      <h2>My Component</h2>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default MyComponent;
