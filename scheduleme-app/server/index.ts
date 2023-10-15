89c18d9c06d645d036bb6637f42eb2869b8a9a19
jgreener6 <justinmgreen06@gmail.com>
2023-10-15 15:27:21 -0400

    Updates to backend and frontend integration

--- scheduleme-app/server/index.ts
+++ scheduleme-app/server/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import authRoutes from './auth';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use('/auth', authRoutes);
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json()); // to be able to access req.body
 
 
 app.get('/', (req, res) => {
--- scheduleme-app/src/Components/AuthenticationAndRegristration.tsx
+++ scheduleme-app/src/Components/AuthenticationAndRegristration.tsx
@@ -1,2 +1,22 @@
-// emptyModule.ts
-export default {};
+import axios from 'axios';
+
+const registerUser = async (userData) => {
+  try {
+    const response = await axios.post('/auth/register', userData);
+    console.log(response.data);
+    // Handle success, maybe redirect user or show a message
+  } catch (error) {
+    console.error('Error registering user:', error);
+    // Handle error, maybe show an error message to the user
+  }
+}
+const loginUser = async (userData) => {
+    try {
+      const response = await axios.post('/auth/login', userData);
+      const { token } = response.data;
+      // Store the token, maybe in React state, localStorage, or Context
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  }
+  
