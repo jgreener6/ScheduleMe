49d4fd55595904bc4b5b0ab3792ac7230d025961
jgreener6 <justinmgreen06@gmail.com>
2023-10-21 11:53:11 -0400

    Integrated the authentication into app

--- scheduleme-app/src/App.tsx
+++ scheduleme-app/src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { Layout, Button } from 'antd';
+import { Layout } from 'antd';
 import './App.css';
 import NavigationMenu from './Components/NavigationMenu';
-import CalendarComponent from './Components/Calendar'; // Import your calendar component
-import JobPostingForm from './Components/JobPostingForm'; // Import the JobPostingForm component
-import MessageBoardComponent from './Components/MessageBoardComponent'; // Import your MessageBoardComponent
+import CalendarComponent from './Components/Calendar';
+import JobPostingForm from './Components/JobPostingForm';
+import MessageBoardComponent from './Components/MessageBoardComponent';
 import OverviewDashboard from './Components/OverviewDashboard';
-
-
+import AuthenticationAndRegistration from './Components/AuthenticationAndRegristration'; // Import the AuthenticationAndRegistration component
 
 const { Header, Content } = Layout;
 
@@ -15,47 +14,56 @@ const App: React.FC = () => {
   const [selectedKeys, setSelectedKeys] = useState<string[]>(['1']);
   const [openKeys, setOpenKeys] = useState<string[]>(['sub1']);
   const [showJobPostingForm, setShowJobPostingForm] = useState<boolean>(false);
-
-  const [selectedComponent, setSelectedComponent] = useState<string | null>(null); // Declare selectedComponent
+  const [selectedComponent, setSelectedComponent] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false); // State to track user authentication
 
   const handleMenuSelect = (selectedKeys: string[]) => {
     setSelectedKeys(selectedKeys);
-    
-    // Determine which component to display based on the selected menu option
+
     if (selectedKeys.includes('calendar')) {
       setSelectedComponent('calendar');
     } else if (selectedKeys.includes('newJob')) {
       setSelectedComponent('newJob');
-    } else {selectedKeys.includes('messageBoard')
-      setSelectedComponent('messageBoard'); // Handle other menu options here
+    } else if (selectedKeys.includes('messageBoard')) {
+      setSelectedComponent('messageBoard');
     }
-    console.log('selectedComponent:', selectedComponent);
-
   };
 
   const handleShowJobPostingForm = () => {
     setShowJobPostingForm(true);
   };
 
+  // Simple authentication handling (for demonstration purposes)
+  const handleAuthentication = () => {
+    setIsAuthenticated(true);
+  };
+
   return (
     <Layout>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
-  
+        {/* You can add header content or a logo here */}
       </Header>
       <Layout>
-        <NavigationMenu
-          selectedKeys={selectedKeys}
-          openKeys={openKeys}
-          onMenuSelect={handleMenuSelect}
-        />
-        <Layout style={{ padding: '0 24px 24px' }}>
-          {selectedComponent === 'calendar' && <CalendarComponent />}
-          {selectedComponent === 'newJob' && <JobPostingForm />} {/* Render JobPostingForm */}
-          {selectedComponent === 'messageBoard' && <MessageBoardComponent />} {/* Render MessageBoardComponent */}
-          {!selectedComponent && <OverviewDashboard />} {/* Default to OverviewDashboard when no other component is selected */}
-
-          {/* Add other components based on the selected menu option */}
-        </Layout>
+        {!isAuthenticated ? (
+          <Content style={{ padding: '0 24px 24px' }}>
+            {/* Display the AuthenticationAndRegistration component when not authenticated */}
+            <AuthenticationAndRegistration onLogin={handleAuthentication} />
+          </Content>
+        ) : (
+          <>
+            <NavigationMenu
+              selectedKeys={selectedKeys}
+              openKeys={openKeys}
+              onMenuSelect={handleMenuSelect}
+            />
+            <Layout style={{ padding: '0 24px 24px' }}>
+              {selectedComponent === 'calendar' && <CalendarComponent />}
+              {selectedComponent === 'newJob' && <JobPostingForm />}
+              {selectedComponent === 'messageBoard' && <MessageBoardComponent />}
+              {!selectedComponent && <OverviewDashboard />}
+            </Layout>
+          </>
+        )}
       </Layout>
     </Layout>
   );
--- scheduleme-app/src/Components/AuthenticationAndRegristration.tsx
+++ scheduleme-app/src/Components/AuthenticationAndRegristration.tsx
@@ -1,28 +1,50 @@
-import axios from 'axios';
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-type UserData = {
-    username: string;
-    password: string;
-    email: string;  
-  }
-  
+type SignInProps = {
+  onLogin?: (userData: {username: string, password: string}) => void;
+};
 
-  const registerUser = async (userData: UserData) => {
-    try {
-      const response = await axios.post('/auth/register', userData);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error registering user:", error);
+const SignIn: React.FC<SignInProps> = ({ onLogin }) => {
+  const onFinish = (values: any) => {
+    if (onLogin) {
+      onLogin(values);
     }
-  }
-  
-  const loginUser = async (userData: UserData) => {
-    try {
-      const response = await axios.post('/auth/login', userData);
-      const { token } = response.data;
-      // Do something with the token...
-    } catch (error) {
-      console.error("Error logging in:", error);
-    }
-  }
-  
+  };
+
+  return (
+    <Form
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default SignIn;
